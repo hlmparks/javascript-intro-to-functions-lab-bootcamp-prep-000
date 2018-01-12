c1cec32ba14acc38267a2624af516eef5d003b43
Heather Parks <hlmparks@gmail.com>
2018-01-12 01:39:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if string.toLowerCase === string
+  if (string.toLowerCase === string) {
     console.log("I can't hear you!")
-}
+  }
+  else if (string.toUpperCase === string) {
+    console.log("YES INDEED!")
+  }
