e937c22141c50d46bf6ecb0dc3a3262ccc1bdc01
Heather Parks <hlmparks@gmail.com>
2018-01-12 02:00:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function sayHiToGrandma(string) {
     return("I can't hear you!")
   }
   else if (string.toUpperCase() === string) {
-    console.log("YES INDEED!")
+    return("YES INDEED!")
   }
   else ("I love you, Grandma.")
-    console.log("I love you, too.")
+    return("I love you, too.")
 }
